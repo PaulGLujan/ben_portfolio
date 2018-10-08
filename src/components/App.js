273be2ca51bd034e0c9505e77a6c11f472c0b33e
Paul Lujan <paulglujan@gmail.com>
2018-10-08 16:21:47 -0700

    Added picture show placeholder

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import NavColumn from './nav_column';
+import PictureShow from './picture_show';
 
 export default ()=>(
   <div className="container">
     <h1>Ben L. Nguyen</h1>
     <NavColumn/>
+    <PictureShow/>
   </div>
 )
---
+++ src/components/picture_show.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default ()=>(
+  <div className="container">
+    <h1>Picture show goes here.</h1>
+  </div>
+)
