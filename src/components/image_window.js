2531cfa4f17336fc4f194da6994e81a0fa704975
Paul Lujan <paulglujan@gmail.com>
2018-10-08 16:25:44 -0700

    Made image window component

---
+++ src/components/image_window.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ImageWindow extends Component {
+  render(){
+    return(
+      <h1>Image Window spot</h1>
+    )
+  }
+}
+
+export default ImageWindow;
--- src/components/picture_show.js
+++ src/components/picture_show.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import ImageWindow from './image_window';
 
 export default ()=>(
   <div className="container">
     <h1>Picture show goes here.</h1>
+    <ImageWindow/>
   </div>
 )
