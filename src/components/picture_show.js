e6f481e890a3f5c9ccab6896707fb40b32620cce
Paul Lujan <paulglujan@gmail.com>
2018-10-09 15:45:22 -0700

    Rendered image toggle component

--- src/components/picture_show.js
+++ src/components/picture_show.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ImageWindow from './image_window';
 import Caption from './caption';
+import ImageToggle from './image_toggle'
 
 export default ()=>(
   <div className="container">
     <h1>Picture show goes here.</h1>
     <ImageWindow/>
     <Caption/>
+    <ImageToggle/>
   </div>
 )
