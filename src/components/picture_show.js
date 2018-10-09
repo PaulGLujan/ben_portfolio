f11ef676b2c72bf7dfc53257fc7c3ef039c83b8f
Paul Lujan <paulglujan@gmail.com>
2018-10-09 15:56:16 -0700

    Created and rendered thumb nail reel component

--- src/components/picture_show.js
+++ src/components/picture_show.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ImageWindow from './image_window';
 import Caption from './caption';
 import ImageToggle from './image_toggle'
+import ThumbnailReel from './thumbnail_reel';
 
 export default ()=>(
   <div className="container">
@@ -9,5 +10,6 @@ export default ()=>(
     <ImageWindow/>
     <Caption/>
     <ImageToggle/>
+    <ThumbnailReel/>
   </div>
 )
---
+++ src/components/thumbnail_reel.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ThumbnailReel extends Component {
+  render(){
+    return(
+      <h1>Thumbnail Reel</h1>
+    )
+  }
+}
+
+export default ThumbnailReel;
