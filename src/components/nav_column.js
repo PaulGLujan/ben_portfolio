b34b9c74498554861b927d14a9999f11db7c7954
Paul Lujan <paulglujan@gmail.com>
2018-10-09 16:06:19 -0700

    Moved navbar to left

--- src/components/nav_column.js
+++ src/components/nav_column.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class NavColumn extends Component {
   render(){
     return(
-      <div className="container">
+      <div className="container col-2 row justify-content-start">
         <h3 className='mt-1'>Portraits</h3>
         <h3 className='mt-1'>Landscapes</h3>
         <h3 className='mt-1'>Contact</h3>
