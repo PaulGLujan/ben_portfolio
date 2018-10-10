269dc4085658cace3e773b376f80978725c7db29
Paul Lujan <paulglujan@gmail.com>
2018-10-10 15:58:05 -0700

    Rendered slide component

--- src/components/thumbnail_reel.js
+++ src/components/thumbnail_reel.js
@@ -4,14 +4,30 @@ import welding from '../images/welding.png';
 import food from '../images/food.jpg';
 import guitar from '../images/guitar.jpg';
 import suit from '../images/suit.jpg';
-import { Carousel } from 'react-responsive-carousel';
+import Slider from "react-slick";
+
 
 class ThumbnailReel extends Component {
   render(){
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 3
+    };
     return(
-      <div className="container">
-        <h1>Oh thumb reel</h1>
-      </div>
+      <Slider {...settings}>
+        <div>
+          <img src={nepal} alt="nepal"/>
+        </div>
+        <div>
+          <img src={welding} alt="welding" />
+        </div>
+        <div>
+          <img src={food} alt="food" />
+        </div>
+      </Slider>
     )
   }
 }
