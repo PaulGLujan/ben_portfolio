6e3f7fe9ef02c62cd85a632a832fef6cb0ffc92b
Paul Lujan <paulglujan@gmail.com>
2018-10-10 15:42:45 -0700

    npm installed carousel

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
