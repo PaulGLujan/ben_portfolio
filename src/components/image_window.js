085949cb6f153d24a1756276f3447847bd7859f7
Paul Lujan <paulglujan@gmail.com>
2018-10-10 14:50:30 -0700

    Added more pictures and a selectedItem index

--- src/components/image_window.js
+++ src/components/image_window.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
 import nepal from '../images/nepal.png';
 import welding from '../images/welding.png';
+import food from '../images/food.jpg';
+import guitar from '../images/guitar.jpg';
 import { Carousel } from 'react-responsive-carousel';
 
 class ImageWindow extends Component {
   render(){
     return(
       <div className="container">
-        <Carousel showIndicators={false} showThumbs={false}>
+        <Carousel showIndicators={false} showThumbs={false} selectedItem={0}>
           <div>
             <img src={nepal}/>
           </div>
           <div>
             <img src={welding}/>
+          </div>
+          <div>
+            <img src={food} />
+          </div>
+          <div>
+            <img src={guitar} />
           </div>
         </Carousel>
       </div>
