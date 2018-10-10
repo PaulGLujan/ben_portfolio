b606de289e7b553dc0bbfe0574095f69d6f9ad72
Paul Lujan <paulglujan@gmail.com>
2018-10-10 14:33:01 -0700

    Rendered carousel

--- src/components/image_window.js
+++ src/components/image_window.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 import nepal from '../images/nepal.png';
+import welding from '../images/welding.png';
+import { Carousel } from 'react-responsive-carousel';
 
 class ImageWindow extends Component {
   render(){
     return(
       <div className="container">
+        <Carousel>
+          <div>
+            <img src={nepal}/>
+            <p className="legend">Legend 1</p>
+          </div>
+          <div>
+            <img src={welding}/>
+            <p className="legend">Legend 2</p>
+          </div>
+        </Carousel>
         <img src={nepal} alt="Nepal" className='col-12'/>
       </div>
     )
