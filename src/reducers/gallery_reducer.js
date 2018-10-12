146dd4fe8479c0661d6105925bb0a80f9a25f280
Paul Lujan <paulglujan@gmail.com>
2018-10-12 12:12:06 -0700

    Combined gallery reducer with other reducers

--- src/reducers/gallery_reducer.js
+++ src/reducers/gallery_reducer.js
@@ -1,7 +1,8 @@
 import types from "../actions/types";
+import portraits from "../assets/images/portraits/portraits";
 
 const DEFAULT_STATE = {
-  position: 0,
+  gallery: portraits,
 }
 
 export default (state=DEFAULT_STATE, action)=>{
