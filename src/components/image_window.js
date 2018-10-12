bd6dffb9f0ab552d7bda3ab40a231460d18d9336
Paul Lujan <paulglujan@gmail.com>
2018-10-12 15:58:34 -0700

    Rendered slick carousel

--- src/components/image_window.js
+++ src/components/image_window.js
@@ -14,6 +14,13 @@ class ImageWindow extends Component {
     return <ImageDiv src={src} alt={alt} name={name} key={key}/>
   }
   render(){
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     const galArr = this.props.gallery;
     const outputArr = [];
     for (let i=0; i<galArr.length; i++){
@@ -22,7 +29,9 @@ class ImageWindow extends Component {
     return(
       <div className="container imageWindow">
         {/* <Carousel showIndicators={false} showThumbs={false} selectedItem={parseInt(this.props.position)}> */}
+        <Slider {...settings}>
           {outputArr}
+        </Slider>
         {/* </Carousel> */}
       </div>
     )
