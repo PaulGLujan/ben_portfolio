461767796992f6b620ef7be6e5a3e0256cd64955
Paul Lujan <paulglujan@gmail.com>
2018-10-11 19:07:21 -0700

    Added event listeners to left and right toggle

--- src/components/image_toggle.js
+++ src/components/image_toggle.js
@@ -2,14 +2,22 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 class ImageToggle extends Component {
+  constructor(props){
+    super(props);
+    this.toggleLeft = this.toggleLeft.bind(this);
+    this.toggleRight = this.toggleRight.bind(this);
+  }
   toggleLeft(){
-    console.log(this.props.position);
+    console.log('left', this.props.position);
+  }
+  toggleRight() {
+    console.log('right', this.props.position);
   }
   render(){
     return(
       <div className="container col-4 row justify-content-between">
-        <h3><i className="fas fa-caret-left"></i> Previous</h3>
-        <h3>Next <i className="fas fa-caret-right"></i></h3>
+        <h3 onClick={this.toggleLeft}><i className="fas fa-caret-left"></i> Previous</h3>
+        <h3 onClick={this.toggleRight}>Next <i className="fas fa-caret-right"></i></h3>
       </div>
     )
   }
