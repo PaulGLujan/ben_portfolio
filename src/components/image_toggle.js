23803ed9fcd29805697a617c197c7f79efce1d5e
Paul Lujan <paulglujan@gmail.com>
2018-10-13 16:02:39 -0700

    Fixed toggle and gallery alignment

--- src/components/image_toggle.js
+++ src/components/image_toggle.js
@@ -20,7 +20,7 @@ class ImageToggle extends Component {
     return(
       <div className="col-4 row justify-content-between">
         <h3 onClick={this.toggleLeft}><i className="fas fa-caret-left"></i> Previous</h3>
-        <h3 onClick={this.toggleRight}>Next <i className="fas fa-caret-right"></i></h3>
+        <h3 onClick={this.toggleRight} className='mr-4'>Next <i className="fas fa-caret-right"></i></h3>
       </div>
     )
   }
