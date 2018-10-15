efa76ef504dcb58c6b157644726cc89bdac9a335
Paul Lujan <paulglujan@gmail.com>
2018-10-15 11:10:25 -0700

    Added instagram icon with link

--- src/components/nav_column.js
+++ src/components/nav_column.js
@@ -39,6 +39,7 @@ class NavColumn extends Component {
           <h3 className='mt-1 pointer-cursor' onClick={landscapesClick}>Landscapes</h3>
           <h3 className='mt-1 pointer-cursor' onClick={portraitClick}>Portraits</h3>
           <h3 className='mt-1 pointer-cursor' onClick={contactClick}>Contact</h3>
+          <h3><a target='_blank' href="https://www.google.com/"><i class="fab fa-instagram"></i></a></h3>
         </div>
       </div>
     )
