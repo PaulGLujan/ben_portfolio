dcf6eaa00beaf80543275880cecac1d70801e285
Paul Lujan <paulglujan@gmail.com>
2018-10-14 22:05:55 -0700

    Contact section appears on click

--- src/reducers/main_content_reducer.js
+++ src/reducers/main_content_reducer.js
@@ -8,6 +8,8 @@ const DEFAULT_STATE = {
 
 export default (state = DEFAULT_STATE, action) => {
   switch (action.type) {
+    case types.UPDATE_MAIN_SECTION:
+      return { ...state, mainContent: action.payload }
     default:
       return state;
   }
