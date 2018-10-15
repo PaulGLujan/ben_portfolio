561a4480020623f47fb291ef70150ac32538f527
Paul Lujan <paulglujan@gmail.com>
2018-10-15 12:25:32 -0700

    Made contact form its own component and made a new component for contact section

--- src/components/contact_section.js
+++ src/components/contact_section.js
@@ -1,58 +1,6 @@
-import React, {Component} from 'react';
-import Field from './contact_form_field';
+import React from 'react';
+import ContactForm from './contact_form';
 
-class contactSection extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      form: {
-        firstName: '',
-        lastName: '',
-        phone: '',
-        email: ''
-      }
-    }
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleInputChange(event) {
-    const { value, name } = event.target;
-
-    const { form } = this.state;
-
-    form[name] = value;
-
-    this.setState({
-      form: { ...form }
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log('handleSubmit called, form values are:', this.state.form);
-
-    this.props.add(this.state.form);
-  }
-
-  render() {
-    const { firstName, lastName, phone, email } = this.state.form;
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <Field name="firstName" label="Name" type="text" value={firstName} onChange={this.handleInputChange} />
-        <Field name="lastName" label="Email" type="text" value={lastName} onChange={this.handleInputChange} />
-        <Field name="phone" label="Phone" type="tel" value={phone} onChange={this.handleInputChange} />
-        <div class="form-group">
-          <label for="comment">Comments:</label>
-          <textarea class="form-control" rows="5" id="comment"></textarea>
-        </div>
-        <button>Add Contact</button>
-      </form>
-    )
-  }
-}
-
-export default contactSection;
+export default ()=>(
+  <ContactForm/>
+)
