09022851f0f4b8762de0db42b11f91488de940ce
Paul Lujan <paulglujan@gmail.com>
2018-10-14 21:22:20 -0700

    Converted App into class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import NavColumn from './nav_column';
 import PictureShow from './picture_show';
 import './App.css';
 
-export default ()=>(
-  <div className="container">
-    <div className='container row'>
-      <NavColumn />
-      <PictureShow />
-    </div>
-  </div>
-)
+class App extends Component {
+  render(){
+    return(
+      <div className="container">
+        <div className='container row'>
+          <NavColumn />
+          <PictureShow />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
