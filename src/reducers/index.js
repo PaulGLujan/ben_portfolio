6463326c93cccfc916e5a7ae3485f33b88c81be7
Paul Lujan <paulglujan@gmail.com>
2018-10-14 21:19:35 -0700

    Combined main content reducer with all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import picPosReducer from './pic_pos_reducer';
 import galleryReducer from './gallery_reducer';
+import mainContentReducer from './main_content_reducer';
 
 export default combineReducers({
   picturePosition: picPosReducer,
   gallery: galleryReducer,
+  mainContent: mainContentReducer,
 })
