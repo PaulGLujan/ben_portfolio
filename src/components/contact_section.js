3055cd98e2c94b4096e726366f4ce1952bdb8e33
Paul Lujan <paulglujan@gmail.com>
2018-10-15 14:45:34 -0700

    Contact in-progress

--- src/components/contact_section.js
+++ src/components/contact_section.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ContactForm from './contact_form';
 
 export default ()=>(
-  <ContactForm/>
+  <div>
+    <div className="contactSection mt-3">
+      <ContactForm />
+    </div>
+    <h3 className="pseudoToggleBar clearfix mt-1 mb-3">A</h3>
+    {/* <div className="pseudoToggleBar mt-1"></div> */}
+    <div className="contactFooter"></div>
+  </div>
 )
