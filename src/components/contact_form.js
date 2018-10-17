fbc295255b04aad330fc6823bb107f23df29cd40
Paul Lujan <paulglujan@gmail.com>
2018-10-16 18:20:40 -0700

    Positioned contact form

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -41,28 +41,31 @@ class ContactForm extends Component {
     const { firstName, lastName, phone, email } = this.state.form;
 
     return (
-      <div className="contactSection mt-3 row">
-        <div className="container col-6 border-right mt-5 contactForm">
-          <form onSubmit={this.handleSubmit}>
-            <Field name="firstName" label="Name:" type="text" value={firstName} onChange={this.handleInputChange} />
-            <Field name="lastName" label="Email:" type="text" value={lastName} onChange={this.handleInputChange} />
-            <Field name="phone" label="Phone:" type="tel" value={phone} onChange={this.handleInputChange} />
-            <div class="form-group">
-              <label className="row justify-content-end mr-2" for="comment">Comments:</label>
-              <textarea class="form-control" rows="5" id="comment"></textarea>
+      <div className="contactSection mt-3 flex-column justify-content-center d-flex">
+          <div className="row">
+            <div className="col-2"></div>
+            <div className="container col-5 border-right mt-5 contactForm">
+              <form onSubmit={this.handleSubmit}>
+                <Field name="firstName" label="Name:" type="text" value={firstName} onChange={this.handleInputChange} />
+                <Field name="lastName" label="Email:" type="text" value={lastName} onChange={this.handleInputChange} />
+                <Field name="phone" label="Phone:" type="tel" value={phone} onChange={this.handleInputChange} />
+                <div class="form-group">
+                  <label className="row justify-content-end mr-2" for="comment">Comments:</label>
+                  <textarea class="form-control" rows="5" id="comment"></textarea>
+                </div>
+                <div className="row justify-content-end mr-2">
+                  <button>Add Contact</button>
+                </div>
+              </form>
             </div>
-            <div className="row justify-content-end mr-2">
-              <button>Add Contact</button>
+            <div className="container col-5">
+              <h5>Architectural and Landscapes</h5> 
+              <h5>Engagements and Weddings</h5>
+              <h5>Corporate Events</h5>
+              <h5>Profile Pictures</h5>
             </div>
-          </form>
+          </div>
         </div>
-        <div className="container col-6">
-          <h5>Architectural and Landscapes</h5> 
-          <h5>Engagements and Weddings</h5>
-          <h5>Corporate Events</h5>
-          <h5>Profile Pictures</h5>
-        </div>
-      </div>
     )
   }
 }
