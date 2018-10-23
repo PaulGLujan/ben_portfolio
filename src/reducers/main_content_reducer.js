bc737a3b81abcf4f90dd880b92b15c07cda2c934
Paul Lujan <paulglujan@gmail.com>
2018-10-23 15:48:03 -0700

    Renamed mainContent redux state to mainComponent

--- src/reducers/main_content_reducer.js
+++ src/reducers/main_content_reducer.js
@@ -3,13 +3,13 @@ import PictureShow from '../components/picture_show';
 import ContactSection from '../components/contact_section';
 
 const DEFAULT_STATE = {
-  mainContent: PictureShow,
+  mainComponent: PictureShow,
 }
 
 export default (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case types.UPDATE_MAIN_SECTION:
-      return { ...state, mainContent: action.payload }
+      return { ...state, mainComponent: action.payload }
     default:
       return state;
   }
